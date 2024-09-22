29bee5c5d12ee989bc4fec8fb150385847aeb8f0
ji4a <117786715+ji4a@users.noreply.github.com>
2024-09-22 07:06:47 +0200

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo timedatectl set-timezone Europe/Amsterdam
+
+sudo apt update && sudo apt -y install unzip mysql-server
+
+MYSQL_PASSWORD="h3llo"
+sudo mysql -u root --execute="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASSWORD}'; GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES; CREATE DATABASE traccar;"
+
+wget https://www.traccar.org/download/traccar-linux-64-latest.zip
+unzip traccar-linux-*.zip
+sudo ./traccar.run
+
+sudo bash -c "cat > /opt/traccar/conf/traccar.xml << EOF
+<?xml version='1.0' encoding='UTF-8'?>
+
+<!DOCTYPE properties SYSTEM 'http://java.sun.com/dtd/properties.dtd'>
+
+<properties>
+
+    <entry key='database.driver'>com.mysql.cj.jdbc.Driver</entry>
+    <entry key='database.url'>jdbc:mysql://localhost/traccar?zeroDateTimeBehavior=round&amp;serverTimezone=UTC&amp;allowPublicKeyRetrieval=true&amp;useSSL=false&amp;allowMultiQueries=true&amp;autoReconnect=true&amp;useUnicode=yes&amp;characterEncoding=UTF-8&amp;sessionVariables=sql_mode=''</entry>
+    <entry key='database.user'>root</entry>
+    <entry key='database.password'>${MYSQL_PASSWORD}</entry>
+    <entry key='topin.port'>5199</entry>
+
+</properties>
+EOF"
+
+sudo ufw allow 8082/tcp
+sudo ufw allow 5199/tcp
+
+sudo service traccar start
+
+echo "Installation complete. It's popcorn time :)"
